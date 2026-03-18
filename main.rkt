dacfd7253ed8144dc6aee9cc620e468d6dcad3a8
Jace O King <44843586+kingjaceo@users.noreply.github.com>
2026-03-18 09:45:02 -0500

    Update main.rkt

--- main.rkt
+++ main.rkt
@@ -1,3 +1,5 @@
 #lang br/quicklang
-; your main language file goes here
-; see https://beautifulracket.com/basic-3/source-listing.html for examples
+
+(module reader br
+  (require "reader.rkt")
+  (provide read-syntax))
