49e70a6d01a5263669d7feb3bc159768c8389ac5
Jace O King <44843586+kingjaceo@users.noreply.github.com>
2026-03-18 09:46:16 -0500

    Update reader.rkt

--- reader.rkt
+++ reader.rkt
@@ -1,8 +1,6 @@
 #lang br/quicklang
 (require "tokenizer.rkt" "parser.rkt")
 
-; your reader goes here!
-; use your tokenizer and parser to define a read-syntax function
 (define (read-syntax path port)
   'todo)
 
