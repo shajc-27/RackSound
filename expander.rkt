0a33a7d04780d31c3eabf6c5cf80e61d6aedce6f
Charles Reed <reecw-27@rhodes.edu>
2026-04-06 10:46:21 -0500

    Worked on Reader and Expander

--- expander.rkt
+++ expander.rkt
@@ -1,3 +1,10 @@
 #lang br/quicklang
+;require rsound
+
+(define-macro (stacker-module-begin HANDLE-EXPR ...)
+              #'(#%module-begin
+                 'HANDLE-EXPR ...))
+
+(provide (rename-out [stacker-module-begin #%module-begin]))
 
 ; your expander goes here!
