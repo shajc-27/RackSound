7866c2d612aa52e0feec8f9e9384e9cf5621b87f
Charles Reed <reecw-27@rhodes.edu>
2026-04-08 13:31:10 -0500

    moving reader.rkt to main

--- tokenizer.rkt
+++ tokenizer.rkt
@@ -1,7 +1,16 @@
 #lang br/quicklang
-(require brag/support)
+(require "parser.rkt") 
 
+#|
 (define (make-tokenizer port)
   'todo)
 
 (provide make-tokenizer)
+|#
+
+(define (read-syntax path port)
+  (define parse-tree (parse path (make-tokenizer port))) 
+  (define module-datum `(module bf-mod "expander.rkt"
+                          ,parse-tree))
+  (datum->syntax #f module-datum))
+(provide read-syntax)
