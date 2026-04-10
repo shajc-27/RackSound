a095cc1b0502f1034af4b546c6965850583aa95f
Charles Reed <reecw-27@rhodes.edu>
2026-04-10 13:34:11 -0500

    adding tests.

---
+++ parse-only-test.rkt
@@ -0,0 +1,9 @@
+#lang basic/parse-only 
+
+INSTRUMENT va "STRING" 
+
+PLAY va E5
+REPEAT 5 
+  PLAY [va E5]
+END 
+
---
+++ tokenize-only.rkt
@@ -0,0 +1,13 @@
+#lang br/quicklang 
+(require brag/support "tokenizer.rkt") 
+
+(define (Read-syntax path port) 
+  (define tokens (apply-tokenizer make-tokenizer port)) 
+  (strip-bindings 
+    #`(module basic-tokens-mod basic/tokenize-only
+        #,@tokens)))
+(module+ reader (provide read-syntax)) 
+
+(define-macro (tokenize-only-mb TOKEN ...)
+              #'(#%module-begin (list TOKEN ...))) 
+(provide (rename-out [tokenize-only-mb #%module-begin]))
