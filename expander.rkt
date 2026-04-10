37cdfab0bb1fe1895d07d5e949821c84e1eb319f
JShapley <shapleyjackson@gmail.com>
2026-04-10 12:50:31 -0500

    added comments from class 4/10/26

--- expander.rkt
+++ expander.rkt
@@ -1,21 +1,21 @@
 #lang br/quicklang
 (provide (matching-identifiers-out #rx"^b-" (all-defined-out))) ; provides all macros/functions starting with "b-"
 
-(define-macro (b-line NUM STATEMENT ...)
+(define-macro (b-line NUM STATEMENT ...) ;provides macro for each line for parse tree below
   (with-pattern ([LINE-NUM (prefix-id "line-" #'NUM
                                       #:source #'NUM)]) ; assigns LINE-NUM's srcloc to be NUM's srcloc
     (syntax/loc caller-stx                              ; assigns this new syntax's srcloc to be b-line's srcloc
-      (define (LINE-NUM) (void) STATEMENT ...))))
+      (define (LINE-NUM) (void) STATEMENT ...)))) ; expands every line into a FUNCTION
 
 (define-macro (b-module-begin (b-program LINE ...))
   (with-pattern
       ([((b-line NUM STMT ...) ...) #'(LINE ...)]
        [(LINE-FUNC ...) (prefix-id "line-" #'(NUM ...))])
     #'(#%module-begin
-       LINE ...   ; we can put in a SHIMS ' to see what the expander is producing
+       LINE ...   ; we can put in a SHIMS ' to see what the expander is producing ; this is for REPEATS
        (define line-table
          (apply hasheqv (append (list NUM LINE-FUNC) ...))) ; we can also add code inside this macro
-       (void (run line-table)))))
+       (void (run line-table))))) ;run is what runs the line funcs
 (provide (rename-out [b-module-begin #%module-begin]))
 
 (struct end-program-signal ())
@@ -46,8 +46,9 @@
         (line-func)
         (add1 line-idx)))))
 
-(define (b-rem val) (void))
-(define (b-print . vals)
+; all of these are funcs, not macros
+(define (b-rem val) (void)) ; do nothing 
+(define (b-print . vals)    ; print all vals(?)
   (displayln (string-append* (map ~a vals))))
 (define (b-sum . vals) (apply + vals))
 (define (b-expr expr)
