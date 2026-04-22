5daa67ff3281d618c6826d86d1ee4a86cd2b4a16
Scarlet-Phoenix <cwreed2005@gmail.com>
2026-04-22 15:35:16 -0500

    Update parser.rkt
    Preparing a last ditch effort to have ***SOMETHING*** to show

--- racket_sound/parser.rkt
+++ racket_sound/parser.rkt
@@ -1,29 +1,50 @@
 #lang brag
-rsnd-program : [rsnd-line] (/NEWLINE [rsnd-line])*
-@rsnd-line : [rsnd-statement | rsnd-dec]  ;([rsnd-statement] [rsnd-inst | rsnd-strum | rsnd-tone])*
-@rsnd-statement : play | stop | speed |rsnd-loop
-@rsnd-dec : inst
-@rsnd-loop: repeat /NEWLINE (rsnd-line [/NEWLINE])* repend
 
-@rsnd-line-num : INTEGER
-play   : /"PLAY" [rsnd-tone] (/COMMA [rsnd-tone])*
-stop   : /"STOP"
-speed  : /"SPEED" ;SPEED vs BPM?
+racksound : [instrument] [visu-defn] vizualize
+instrument : [wavetype] [frequency] [FILTER]
+; system
+inst-defn: /INST ID /LBRACE (features)* /RBRACE
+@features: wavetype | frequency | filter
+filter: /FILTER /COLON STRING
+frequency : /FREQUENCY /COLON INTEGER
+wavetype : /WAVETYPE /COLON STRING
+
+
+visu-defn: /VISUALIZER /LBRACE (visu-configs)* /RBRACE
+@visu-configs: width | height | title | type
+width: /WIDTH /COLON INTEGER
+height: /HEIGHT /COLON INTEGER
+title : /TITLE /COLON STRING
+type : /TYPE /COLON STRING
+
+vizualize : /VISUALIZE
+
+#|rsnd-program : [rsnd-line] (/NEWLINE [rsnd-line])*
+;@rsnd-line : [rsnd-statement]  ;([rsnd-statement] [rsnd-inst | rsnd-strum | rsnd-tone])*
+;@rsnd-statement : play | stop | speed |rsnd-loop
+;@rsnd-dec : inst
+;@rsnd-loop: "REPEAT" INTEGER /NEWLINE (rsnd-line [/NEWLINE])* "REPEND" 
+
+;@rsnd-line-num : INTEGER
+;play   : /"PLAY" [rsnd-tone] (/COMMA [rsnd-tone])*
+;stop   : /"STOP"
+;speed  : /"SPEED" ;SPEED vs BPM?
 ;playTogether : /"*PLAY" [rsnd-tone] /COMMA ([rsnd-tone])*
-repeat : /"REPEAT" INTEGER
-repend : /"REPEND" 
-inst   : /"INSTRUMENT" [rsnd-inst]
-bpm    : /"SET BPM" INTEGER
+;repeat : /"REPEAT" INTEGER
+;epend : /"REPEND" 
+;inst   : /"INSTRUMENT" [rsnd-inst]
+;bpm    : /"SET BPM" INTEGER
 
 ;@rsnd-inst : STRUMTYPE
 ; alt inst definition -
-@rsnd-inst     : [rsnd-inst-ref] [rsnd-strum]
-@rsnd-inst-ref : STRING
-@rsnd-strum    : STRUMTYPE
+;@rsnd-inst     : [rsnd-inst-ref] [rsnd-strum]
+;@rsnd-inst-ref : STRING
+;@rsnd-strum    : STRING
+ 
 
  ; ignore in expander
 ;@rsnd-tone : STRUMTYPE NOTE
-@rsnd-tone : rsnd-inst NOTE
+;@rsnd-tone : STRING
 ; @rsnd-note : STRING
 ;@rsnd-rem : [rsnd-tone] | INTEGER | VOID ; unsure what this bit even is
 ;@rsnd-tone : inst [rsnd-note] ;@rsnd-tone : (/"[" [inst] [rsnd-note] /"]")
