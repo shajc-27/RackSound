86e343c5b54306b6a47baf2c3aedfa6d271c551c
JShapley <shapleyjackson@gmail.com>
2026-05-01 18:24:09 -0500

    trying to unscrew some errors from the merge

--- racket_sound/lexer.rkt
+++ racket_sound/lexer.rkt
@@ -1,101 +1,40 @@
-#lang br
+#lang br 
 (require brag/support)
-
-(define lexer
-  (lexer-srcloc
-   ;; Whitespace (except newlines - newlines are significant)
-   [whitespace (token lexeme #:skip? #t)]   ; or skip entirely
-   ;; Significant newline
-   ["\n"
-    (token 'NEWLINE lexeme)]
-
-   ;; Comments (assuming ; starts a comment until end of line)
-   [(:: #\; (repetition 0 +inf.0 (char-complement #\newline)))
-    (token 'COMMENT lexeme)]
-
-   ;; Keywords (case-sensitive? Assuming uppercase as shown)
-   ["PLAY"       (token 'PLAY)]
-   ["STOP"       (token 'STOP)]
-   ["SPEED"      (token 'SPEED)]
-   ["PLAY*"      (token 'PLAY-TOGETHER)]   ; or 'PLAY*
-   ["REPEAT"     (token 'REPEAT lexeme)]
-   ["END"        (token 'END)]
-   ["INSTRUMENT" (token 'INSTRUMENT)]
-   ["SET BPM"    (token 'SET-BPM)]         ; note the space
-
-   ;; Punctuation
-   [#\[          (token 'LBRACKET)]
-   [#\]          (token 'RBRACKET)]
-   [#\,          (token 'COMMA)]
-
-   ;; Integers
-   [(:: (char-range #\0 #\9)
-        (repetition 0 +inf.0 (char-range #\0 #\9)))
-    (token 'INTEGER (string->number lexeme))]
-
-   ;; Instrument / Note strings (sequences of non-whitespace, non-special chars)
-   ;; This is very flexible as per your grammar (STRING)
-   [(:or (from/to "\"" "\"") (from/to "'" "'"))
-     (token 'STRING
-            (substring lexeme
-                        1 (sub1 (string-length lexeme))))]
-   ))
-(provide lexer)
-
-
-
-
-
-;#lang br 
-;(require brag/support)
 
 ;(define-lex-abbrev keyfamily (:+ (char-set "ABCDEFG")))
 ;(define-lex-abbrev octave (:+ (char-set "12345678")))
 ;we're doing octaves 0-8 here.
-;(define-lex-abbrev note
-;  (:: (:or (char-set "ABCDEFG")) (:or (char-set "12345678"))))
-;(define-lex-abbrev strum (:or "TRUMPET" "PIANO" "DRUM" "FLUTE" "PURE"))
-;(define-lex-abbrev statement (:or "INSTRUMENT" "PLAY" "STOP" "SPEED" "REPEAT" "END"))
+(define-lex-abbrev note
+  (:: (:or (char-set "ABCDEFG")) (:or (char-set "12345678"))))
+(define-lex-abbrev strum (:or "PIANO" "DRUM" "SINE"))
 
-<<<<<<< HEAD
-(define lexer
+(define basic-lexer
   (lexer-srcloc
     ["\n" (token 'NEWLINE lexeme)]
     [whitespace (token lexeme #:skip? #t)]
-    [(from/stop-before ";" "\n") (token 'COMMENT lexeme)]
-    
-    [(:or "INSTRUMENT" "PLAY" "STOP" "SPEED" "REPEAT" "END" "PLAY*") (token lexeme lexeme)]
+    [(from/stop-before ";" "\n") (token lexeme #:skip? #t)]
+    ;[(:or "INSTRUMENT" "PLAY" "STOP" "SPEED" "REPEAT" "END" "PLAY*") (token lexeme lexeme)]
     [strum     (token 'STRUMTYPE lexeme)]
     [note      (token 'NOTE lexeme)]
     [(:+ (char-set "0123456789")) (token 'INTEGER (string->number lexeme))]
+    [#\[          (token 'LBRACKET)]
+    [#\]          (token 'RBRACKET)]
+    [#\,          (token 'COMMA)]
+    ["INSTRUMENT" (token 'INSTRUMENT lexeme)]
+    ["PLAY" (token 'PLAY lexeme)]
+    ["STOP" (token 'STOP lexeme)]
+    ["SPEED" (token 'SPEED lexeme)]
+    ["REPEAT" (token 'REPEAT lexeme)]
+    ["REPEND" (token 'REPEND lexeme)]
+    ;["*PLAY" (token 'PLAYTOGETHER lexeme)]
+    ["," (token 'COMMA lexeme)]
 
+    
     [(:or (from/to "\"" "\"") (from/to "'" "'"))
      (token 'STRING
             (substring lexeme
-                        1 (sub1 (string-length lexeme))
-    ))]
+                        1 (sub1 (string-length lexeme)
+    )))]
 ))
-=======
-;(define lexer
-;  (lexer-srcloc
-   
- ;   ["\n" (token 'NEWLINE lexeme)]
- ;   [whitespace (token lexeme #:skip? #t)]
- ;   [(from/stop-before ";" "\n") (token 'COMMENT lexeme)]
- ;   [strum (token lexeme)]
-    ;[statement (token lexeme lexeme)]
- ;   [strum (token 'STRUMTYPE lexeme)]
- ;   [note (token 'NOTE lexeme)]
- ;   [(from/stop-before statement " ") (token 'STATMENT lexeme)]
- ;   [(:+ (char-set "0123456789")) (token 'INTEGER (string->number lexeme))]
- ;   [(:or "[" "]")  lexeme] 
-    
-;    [(:or (from/to "\"" "\"") (from/to "'" "'"))
-;     (token 'STRING
- ;           (substring lexeme
-;                        1 (sub1 (string-length lexeme))
-  ;  ))]
-;))
->>>>>>> f4df69d7625b2fb1de0a72bb5b43cf285b58c23e
 
-;(provide lexer)
+(provide basic-lexer)
--- racket_sound/parser.rkt
+++ racket_sound/parser.rkt
@@ -1,79 +1,28 @@
 #lang brag
 rsnd-program : [rsnd-line] (/NEWLINE [rsnd-line])*
-<<<<<<< HEAD
-@rsnd-line : [rsnd-statement] [rsnd-comment]
-@rsnd-statement : play | stop | speed | playTogether | repeat | repend | inst
+@rsnd-line : [rsnd-statement | rsnd-dec]  ;([rsnd-statement] [rsnd-inst | rsnd-strum | rsnd-tone])*
+@rsnd-statement : play | stop | speed |rsnd-loop
+@rsnd-dec : inst
+@rsnd-loop: repeat /NEWLINE (rsnd-line [/NEWLINE])* repend
 
-
-play   : /"PLAY" [rsnd-tone]
-stop   : /"STOP" INTEGER 
-speed  : /"SPEED" INTEGER  ;SPEED vs BPM?
-playTogether : /"PLAY*" [rsnd-tone] ([rsnd-tone])*
+@rsnd-line-num : INTEGER
+play   : /"PLAY" [rsnd-tone] (/COMMA [rsnd-tone])*
+stop   : /"STOP"
+speed  : /"SPEED" ;SPEED vs BPM?
+;playTogether : /"*PLAY" [rsnd-tone] /COMMA ([rsnd-tone])*
 repeat : /"REPEAT" INTEGER
-repend : /"END" 
+repend : /"REPEND" 
 inst   : /"INSTRUMENT" [rsnd-inst]
 bpm    : /"SET BPM" INTEGER
-
 
 ;@rsnd-inst : STRUMTYPE
 ; alt inst definition -
 @rsnd-inst     : [rsnd-inst-ref] [rsnd-strum]
 @rsnd-inst-ref : STRING
 @rsnd-strum    : STRUMTYPE
-@rsnd-tone : rsnd-inst NOTE ; ;change tone to only be the ref?
+@rsnd-tone : rsnd-inst NOTE
 
-rsnd-comment  : COMMENT  ; ignore in expander
-
-;@rsnd-tone : STRUMTYPE NOTE
+; @rsnd-tone : STRUMTYPE NOTE
 ; @rsnd-note : STRING
-=======
-rsnd-line : (rsnd-statement (rsnd-rem))
-@rsnd-statement : rsnd-play | rsnd-stop | rsnd-speed | rsnd-playTogether | rsnd-repeat | rsnd-repend | rsnd-instrument-dec
-
-rsnd-play   : /"PLAY" [rsnd-tone]
-rsnd-stop   : /"STOP" INTEGER 
-rsnd-speed  : /"SPEED" INTEGER  ;SPEED vs BPM?
-rsnd-playTogether : /"PLAY*" [rsnd-tone] (/","[rsnd-tone])*
-rsnd-repeat : /"REPEAT" INTEGER
-rsnd-repend : /"END" 
-rsnd-instrument-dec   : /"INSTRUMENT" [rsnd-inst]
-rsnd-bpm    : /"SET BPM" INTEGER
-
-rsnd-comment : COMMENT
-
-
-@rsnd-inst : [rsnd-inst-ref] [rsnd-strum]
-@rsnd-inst-ref : STRING
-
-@rsnd-rem : [rsnd-tone] | INTEGER | VOID
-@rsnd-tone (/"[" (rsnd-inst rsnd-note)+ / "]")
-@rsnd-strum : STRING ;etc
-@rsnd-note : STRING
-@rsnd-tone : STRUMTYPE NOTE
-
->>>>>>> f4df69d7625b2fb1de0a72bb5b43cf285b58c23e
 ;@rsnd-rem : [rsnd-tone] | INTEGER | VOID ; unsure what this bit even is
 ;@rsnd-tone : inst [rsnd-note] ;@rsnd-tone : (/"[" [inst] [rsnd-note] /"]")
-
-
-; defining statements
-#|rsnd-line : line-num [statement] [rsnd-arg]*
-;rsnd-program : [rsnd-line] (/NEWLINE [rsnd-line])*
-;rsnd-arg : INTEGER | strum | key 
-;@line-num : INTEGER
-;@statement : inst | play | stop | speed | playTogether | repeat
-
-;inst   : "INSTRUMENT" strum
-;play   : "PLAY" strum key 
-;stop   : "STOP"  INTEGER
-;speed  : "SPEED" INTEGER
-;playTogether  : "PLAY-TOGETHER" strum key strum key 
-;repeat : "REPEAT" INTEGER
-;repend : "END" ;
-
-;key    :  STRING
-;strum  : "TRUMPET" | "PIANO" | "DRUM" | "FLUTE" | "PURE"  ;and so on.
-
-; @ = unwraps a redundant statement line, basically stops from each line being in layered parentheses
-; / = "Ignorer", parser throws away after.
-; to my knowledge '@' is moreso for readability, since it just "hides" the stuff.
