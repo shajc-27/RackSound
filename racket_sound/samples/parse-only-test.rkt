c759adf88356c45eaf9c622786a48a415cba09d5
JShapley <shapleyjackson@gmail.com>
2026-05-01 18:30:26 -0500

    removed the compiled directory since it was on the .gitignore that Prof. King wrote

--- racket_sound/samples/parse-only-test.rkt
+++ racket_sound/samples/parse-only-test.rkt
@@ -17,3 +17,14 @@ PLAY 'ca' A5 G5 D5 5,
 
 
 DISPLAY
+
+; the one that def works
+;#lang racket_sound/parse-only
+;INSTRUMENT 'ca' PIANO
+;INSTRUMENT 'da' PIANO
+;PLAY 'ca' B6
+;PLAY 'ca' E5, 'ra' E5
+;STOP 
+;REPEAT 5
+;PLAY 'ca' A5
+;REPEND
