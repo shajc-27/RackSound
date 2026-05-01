0e4409b2f7da24f5f679685dff7edd73cf86104f
JShapley <shapleyjackson@gmail.com>
2026-05-01 18:38:03 -0500

    also added playback & expander bc they weren't up-to-date? Unsure if that's bc of merge bs or what

--- racket_sound/expander.rkt
+++ racket_sound/expander.rkt
@@ -1,93 +1,39 @@
 #lang br/quicklang
 (provide (matching-identifiers-out #rx"^" (all-defined-out))) ; provides all macros/functions starting with "rsnd-"
 
-; this macro defines new functions of the form line-x where x is the line number
-; from the s-expression (rsnd-line NUM STATEMENT ...)
-; we use with-pattern to create the string line-x using NUM
-; syntax/loc and caller-stx are was of passing around source locations
-; for error handling
-(define-macro (rsnd-line NUM STATEMENT ...) 
-  (with-pattern ([LINE-NUM (prefix-id "line-" #'NUM
-                                      #:source #'NUM)]) ; assigns LINE-NUM's srcloc to be NUM's srcloc
-    (syntax/loc caller-stx                              ; assigns this new syntax's srcloc to be rsnd-line's srcloc
-      (define (LINE-NUM) (void) STATEMENT ...))))
+(require "playback.rkt")
 
-; this macro creates a module starting with #%module-begin, which
-; all top-level modules require
-; it takes in a string that looks like (rsnd-program ...) where ... is a
-; bunch of stuff we're calling LINE
-; the module this macro creates is filled with the LINEs
-; (which, as we know, have the form (rsnd-line ...), which means the rsnd-line macro
-;  will expand them!)
-; then, the macro creates a hash table, which maps line numbers
-; to line function names (like line-20), but line function names aren't
-; by default, which is why we define them in the with-pattern
-; finally, the macro calls (void (run line-table))), which is a function
-; defined later!
-; finally, we rename this module, which is called rsnd-module-begin to #%module-begin
-; so that it becomes the top-level module for the reader
-(define-macro (rsnd-module-begin (rsnd-program LINE ...))
-  (with-pattern ;CHANGE PATTERN?
-      ([((rsnd-line NUM STMT ...) ...) #'(LINE ...)]
-       [(LINE-FUNC ...) (prefix-id "line-" #'(NUM ...))])
+(define-macro (rsnd-program INSTRUCTS ...)
+  (with-pattern
+      ([((inst NAME TYPE) ...) (find-definitions 'inst #'(INSTRUCTS ...))]
+       [((play INST NOTE) ...) (find-definitions 'play #'(INSTRUCTS ...))]
+       )
     #'(#%module-begin
-       LINE ...   ; we can put in a SHIMS ' to see what the expander is producing
-       (define line-table
-         (apply hasheqv (append (list NUM LINE-FUNC) ...))) ; we can also add code inside this macro
-       (void (run line-table)))))
-(provide (rename-out [rsnd-module-begin #%module-begin]))
+       (define instruments
+         (apply hasheq (append (list NAME TYPE) ...)))
+       (define notes
+         (list (cons INST NOTE) ...))
+       ;(displayln instruments)
+       ;(displayln notes)
+       (playback instruments notes)
+       )))
+   
 
-(struct end-program-signal ())
-(struct change-line-signal (val))
+(provide (rename-out [rsnd-program #%module-begin]))
 
-(define (rsnd-end) (raise (end-program-signal))) ; rasie exception!
-(define (rsnd-goto expr) (raise (change-line-signal expr))) ; raise exception!
+(begin-for-syntax
+  (require racket/list)
 
-; he said to drop srcloc, so abandon?
-(define (run line-table)
-  (define line-vec ; create a vector of sorted line numbers: helps us decide where to start and what is next
-    (list->vector (sort (hash-keys line-table) <)))
-  (with-handlers ([end-program-signal? (λ (exn-val) (void))])
-    (for/fold ([line-idx 0]) ; starting line is the line at index 0
-              ([i (in-naturals)] ; loop over the naturals: keep going forever!
-               #:break (>= line-idx (vector-length line-vec))) ; if our line index is out of range, stop!
-      (define line-num (vector-ref line-vec line-idx)) ; lookup the line number using the index
-      (define line-func (hash-ref line-table line-num)) ; lookup the line function using the line number
-      (with-handlers
-          ([change-line-signal?
-            (λ (cls)
-              (define clsv (change-line-signal-val cls))
-              (or
-               (and (exact-positive-integer? clsv)
-                    (vector-member clsv line-vec))
-               (error
-                (format "error in line ~a: line ~a not found"
-                        line-num clsv))))])
-        (line-func) ; call the line func!
-        (add1 line-idx))))) ; increment and return line index!
-; now the handlers:
-; if we get the end-program-signal, call and return void: this terminates execution
-; if we get the change-line-signal:
-;  if the line number isn't real, we throw the error
-;  if the line number is real, the first condition of the OR fires:
-;    that is, both conditions of the AND fire
-;    and Racket returns the value of the LAST condition, assuming it isn't false:
-;      that is, we return the next line index (which we looked up!)
-;      since for/fold is waiting for the next value of line-idx,
-;      this exception changes the line-idx to whatever it should be!
+  (define (unique-ids stxs)
+    (remove-duplicates stxs #:key syntax->datum))
+  
+  (define (find-property which line-stxs) ; adapted from BR's basic find-property, used Claude AI
+    (for/first ([stx (in-list (syntax->list line-stxs))]
+                #:when (eq? which (syntax->datum (car (syntax->list stx)))))
+      (cadr (syntax->list stx))))
 
-
-(define (rsnd-rem val) (void)) ; void doesn't do anything! so when we see (rsnd-rem ...), we just do nothing!
-
-(define-macro (play tone)
-  ; build tone, then play?)
-  ;(play
-)
-
-; Don't think these apply to ours atm
-(define (rsnd-print . vals) ; rsnd-print could have an arbitrary number of values (hence the .)
-  (displayln (string-append* (map ~a vals)))) ; map the values to strings (necessary if they're numbers)
-(define (rsnd-sum . vals) (apply + vals)) ; apply the + function to the values
-(define (rsnd-expr expr)
-  (if (integer? expr) (inexact->exact expr) expr)) ; simple if: numbers become exact numbers
-; otherwise, if expr is something like (rsnd-sum 1 2 3), return expr will return the result of calling rsnd-sum
+  (define (find-definitions which line-stxs)
+    (for/list ([stx (in-list (syntax->list line-stxs))]
+               #:when (and (syntax->list stx)
+                           (eq? which (syntax->datum (car (syntax->list stx))))))
+      stx)))
---
+++ racket_sound/playback.rkt
@@ -0,0 +1,81 @@
+#lang racket
+(require rsound)
+(require rsound/piano-tones)
+
+(struct playback-details (speed volume resample-rate instrument-list))
+
+(define frame-rate 48000)
+
+(define bpm 90)
+
+(define frame-counter 0)
+
+;(define (make-inst-list list)
+; (list (for ([(k v) (in-hash list)])
+;         (cons k v))))
+  
+
+
+(define (make-instrument arg) (displayln "make-instrument: not implemented error")) 
+(define (string->midi input)
+  (let* ([note (substring input 0 1)]
+         [octave (string->number (substring input 1 2))])
+    ;(displayln octave)
+    (cond
+      ([or (< octave 0) (> octave 7)] (display "Error in expander: octave too large or too small. Range is 0-7"))
+      ([equal? note "C"] (+ (* octave 12) 24))
+      ([or [equal? note "Db"] [equal? note "C#"]] (+ (* octave 12) 25))
+      ([equal? note "D"] (+ (* octave 12) 26))
+      ([or [equal? note "Eb"] [equal? note "D#"]] (+ (* octave 12) 27))
+      ([equal? note "E"] (+ (* octave 12) 28))
+      ([equal? note "F"] (+ (* octave 12) 29))
+      ([or [equal? note "F#"] [equal? note "Gb"]] (+ (* octave 12) 30))
+      ([equal? note "G"] (+ (* octave 12) 31))
+      ([or [equal? note "G#"] [equal? note "Ab"]] (+ (* octave 12) 32))
+      ([equal? note "A"] (+ (* octave 12) 33))
+      ([or [equal? note "A#"] [equal? note "Bb"]] (+ (* octave 12) 34))
+      ([equal? note "B"] (+ (* octave 12) 35))
+      (else "UNABLE TO PARSE STRING INTO MIDI")))
+  )
+;all
+#|
+(define (resample-table notein) (displayln "resample-table: not implemented error")) 
+(define (pitch-sound rsnd-in midi) (displayln "pitch-sound: not implemented error"))
+(define (load-in-file-to-rsound path) (displayln "load-in-file-to-rsound: not implemented error"))
+;(define (overlay-sound ref1 t1 ref2 t2) (rs-overlay (pitch-sound ref1 t1) (pitch-sound ref2 t2)))
+(define (overlay-sound . e) (displayln "overlay-sound not implemented error"))  
+(define (set-speed new-bpm) (displayln "set-speed: not implemented error"))
+|#
+
+(define (make-sounds instrument note)
+  (if (equal? note '())
+      null
+      (list
+       ;(displayln (car note))
+       (if (equal? (hash-ref instrument (car (car note))) "PIANO")
+           (resample-to-rate 48000 (piano-tone (string->midi(cdr (car note)))))
+           (resample-to-rate 48000 (synth-note "vgame" 49 (cdr (car note)) 22010))) (make-sounds instrument (cdr note)))))
+
+;(define (perform soundlist)
+ ; (displayln (list-ref soundlist 0))
+  ;(if (null? soundlist)
+   ;   (void)
+    ;  (begin
+     ;   (play (list-ref soundlist 0))
+      ;  (sleep 1)
+       ; (perform (cdr soundlist)))))
+;        (cond
+;          [(rsound? (list-ref soundlist 0)) (begin (play (list-ref soundlist 0)) (sleep 1) (perform (cdr soundlist)))]
+;          [else (displayln "Done" )]))
+;                  
+(define (append-all soundlist)
+  (rs-append* (flatten soundlist)))
+
+(define (playback instruments notes)
+  (play (append-all (make-sounds instruments notes)))
+  
+  )
+  
+
+(provide playback) 
+
